304aaa60924069f1673010d5216ecc618226e4ef
lilKriT <lilKriTwebDev@gmail.com>
2026-07-21 23:29:59 +0200

    Lots of styling.

--- _components/TestimonialsSlider.tsx
+++ _components/TestimonialsSlider.tsx
@@ -8,37 +8,61 @@ const TestimonialsSlider = () => {
   const [index, setIndex] = useState(0);
 
   const nextSlide = () => {
-    // console.log(testimonials.length);
     setIndex((index + 1) % testimonials.length);
   };
 
   const prevSlide = () => {
-    // console.log(testimonials.length);
     setIndex((index - 1 + testimonials.length) % testimonials.length);
   };
 
   return (
-    <div className="bg-blue-200">
+    <div className="bg-blue-200 grid grid-cols-[6fr_5fr]">
+      {/* Quote */}
+      <blockquote className="relative z-10 grid grid-rows-[1fr_auto_1fr] mr-[-4ch] font-light">
+        <span className="row-start-2">{testimonials[index].text}</span>
+        <footer className="row-start-3 flex gap-2">
+          <cite>{testimonials[index].name}</cite>
+          <span>{testimonials[index].title}</span>
+        </footer>
+      </blockquote>
+
       {/* Image */}
-      <div>
+      <div className="relative">
         <Image
           src={testimonials[index].imageURL}
           alt={`Picture of ${testimonials[index].name}`}
           width={540}
           height={540}
-          className="duration-1000 animate-slide"
+          className="w-full
+          duration-1000 animate-slide"
         />
-        <button onClick={prevSlide}>prev</button>
-        <button onClick={nextSlide}>next</button>
+
+        {/* Control Buttons */}
+        <div className="absolute bg-white flex gap-2 rounded-full">
+          <button
+            onClick={prevSlide}
+            className="bg-white pl-4 pr-6 py-3 rounded-l-full cursor-pointer"
+          >
+            <Image
+              src={"/icon-prev.svg"}
+              alt="Previous slide"
+              width={12}
+              height={18}
+            />
+          </button>
+          <button
+            onClick={nextSlide}
+            className="bg-white pl-6 pr-4 py-3 rounded-r-full cursor-pointer"
+          >
+            <Image
+              src={"/icon-next.svg"}
+              alt="Next slide"
+              width={13}
+              height={18}
+            />
+          </button>
+        </div>
       </div>
-      {/* Quote */}
-      <blockquote>
-        {testimonials[index].text}
-        <footer>
-          <cite>{testimonials[index].name}</cite>
-          {testimonials[index].title}
-        </footer>
-      </blockquote>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import TestimonialsSlider from "@/_components/TestimonialsSlider";
 export default function Home() {
   return (
     <section
-      className="min-h-dvh flex justify-center items-center p-24
+      className="min-h-dvh flex justify-center items-center
       bg-[url(/pattern-curve.svg),url(/pattern-bg.svg)] bg-position-[bottom_left,center_right_10%] 
       bg-size-[40%_auto,auto_70%] bg-no-repeat"
     >
-      <div className="">
+      <div className="w-full max-w-6xl">
         <TestimonialsSlider />
       </div>
     </section>
